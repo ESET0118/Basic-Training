4b8c775a8ac3db3ef5f7d360ded52a12b7508727
ESET0118 <prakritish.ghosh@esyasoft.com>
2025-09-05 15:07:43 +0530

    Day 3 Assignment

--- ReactJS/Counter/src/Counter.jsx
+++ ReactJS/Counter/src/Counter.jsx
@@ -1,27 +1,12 @@
 function Counter(){
-  return (
-    <div>
-      <h1 id="count">0</h1>
-      <button onClick={() => {
-        const countElement = document.getElementById('count');
-        var count = parseInt(countElement.innerText);
-        count++;
-        countElement.innerText = count;
-        }}>
-        Increment
-      </button>
-      <br></br>
-      <br></br>
-      <button onClick={() => {
-        const countElement = document.getElementById('count');
-        var count = parseInt(countElement.innerText);
-        count--;
-        countElement.innerText = count;
-        }}>
-        Decrement
-      </button>
-    </div>
-  );
+  const prev={name:'A',age:20};
+  const next={...prev,age:21};
+  console.log(next);
+  next.age=22;
+  console.log(next);
+  console.log(prev);
+
+  return(<></>);
 }
 
-export default Counter
+export default Counter; 
