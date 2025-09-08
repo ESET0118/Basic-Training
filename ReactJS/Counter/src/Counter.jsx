1d2e3e089b2a9847303bedabf4e5f9f809580951
ESET0118 <prakritish.ghosh@esyasoft.com>
2025-09-08 17:11:17 +0530

    Day 6

--- ReactJS/Counter/src/Counter.jsx
+++ ReactJS/Counter/src/Counter.jsx
@@ -1,12 +1,36 @@
 function Counter(){
-  const prev={name:'A',age:20};
-  const next={...prev,age:21};
-  console.log(next);
-  next.age=22;
-  console.log(next);
-  console.log(prev);
+  const msg=`Hello,
+  I am Prakritish,
+  welcome to React world!`;
+  console.log(msg);
 
-  return(<></>);
+  const name="Prakritish";
+  const age=21;
+  const person={name,age};
+  console.log(person.age,person.name);
+
+  const utils={
+    greet(){
+      console.log("Hello, I am a method inside an object");
+    }
+  }
+  utils.greet();
+
+  const items=[1,2,3,4,5];
+  const list=items.map((it)=>it*2);
+  console.log(list);
+
+  const even=items.filter((it)=>it%2===0);
+  console.log(even);
+
+  const sum=items.reduce((acc,v)=>acc+=v,0);
+  console.log(sum);
+
+  const max=items.reduce((acc,v)=> v>acc?v:acc,0);  //if(v>acc) return v; else return acc;
+  console.log(max);
+
+
+  return(<>{msg}</>);
 }
 
 export default Counter; 
